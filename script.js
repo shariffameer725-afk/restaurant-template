265d45df034ff105ed6eb2e1cd210062de9c6030
shariffameer725-afk <shariffameer725@gmail.com>
2026-09-01 00:24:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ console.log("Restaurant Website");
 const button = 
 document.querySelector("#btn");
 
-button.addeventlistener("click",() =>{document.body,style.backgroundColor = 
+button.addeventlistener("click",() =>{document.body.style.backgroundColor = 
     "lightblue";
 });
